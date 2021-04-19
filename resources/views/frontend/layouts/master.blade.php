942dcc0309e63b399f8c586e6d20ef5e66be8755
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-19 09:49:37 +0600

    header middle section added

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -11,7 +11,7 @@
 <body>
     <header class="header">
         <div class="header__top">
-            <div class="container-fluid">
+            <div class="container">
                 <div class="row">
                     <div class="col-lg-6 col-md-6">
                         <div class="header__top__left">
@@ -34,14 +34,34 @@
                                 <a href="#"><i class="fa fa-linkedin"></i></a>
                                 <a href="#"><i class="fa fa-pinterest-p"></i></a>
                             </div>
-
-                            <div class="header__top__right__auth">
-                                <a href="#">
-                                    <i class="fa fa-user"></i>
-                                    Sign In
-                                </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container header__middle">
+            <div class="row">
+                <div class="col-lg-3">
+                    <a href="navbar-brand">
+                        <img src="{{ asset('photos/logo.png') }}" alt="Logo" class="header__middle__logo">
+                    </a>
+                </div>
+                <div class="col-lg-6 d-flex justify-content-center">
+                    <form class="form-inline">
+                        <div class="input-group header__middle__search">
+                            <input type="text" class="form-control" placeholder="Search for products">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="submit"><i class="fa fa-search"></i></button>
                             </div>
                         </div>
+                    </form>
+                </div>
+                <div class="col-lg-3 d-flex justify-content-center align-items-center">
+                    <div class="header__middle__auth">
+                        <a href="#">
+                            <i class="fa fa-user"></i>
+                            Sign In
+                        </a>
                     </div>
                 </div>
             </div>
