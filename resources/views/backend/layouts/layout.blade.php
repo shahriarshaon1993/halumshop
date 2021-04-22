dd020a4869a533aa23340f9905dc0199f1597be2
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-22 10:08:49 +0600

    Admin dashboard template added

---
+++ resources/views/backend/layouts/layout.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>{{ config('app.name') }} - @yield('title')</title>
+
+    <!-- vendor css -->
+    <link href="{{ asset('backend/lib/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/lib/Ionicons/css/ionicons.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/lib/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/lib/rickshaw/rickshaw.min.css') }}" rel="stylesheet">
+
+    <!-- Starlight CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/starlight.css') }}">
+</head>
+
+<body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+    @include('backend.partials.sidebar')
+    <!-- ########## END: LEFT PANEL ########## -->
+
+    <!-- ########## START: HEAD PANEL ########## -->
+    @include('backend.partials.header')
+    <!-- ########## END: HEAD PANEL ########## -->
+
+    <!-- ########## START: RIGHT PANEL ########## -->
+    @include('backend.partials.rightsidebar')
+    <!-- ########## END: RIGHT PANEL ########## --->
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="sl-mainpanel">
+        <nav class="breadcrumb sl-breadcrumb">
+            <a class="breadcrumb-item" href="index.html">Halumshop</a>
+            <span class="breadcrumb-item active">@yield('pagename')</span>
+        </nav>
+
+        <div class="sl-pagebody">
+
+            @yield('content')
+
+        </div><!-- sl-pagebody -->
+
+        @include('backend.partials.footer')
+
+    </div><!-- sl-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+    <script src="{{ asset('backend/lib/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('backend/lib/popper.js/popper.js') }}"></script>
+    <script src="{{ asset('backend/lib/bootstrap/bootstrap.js') }}"></script>
+    <script src="{{ asset('backend/lib/jquery-ui/jquery-ui.js') }}"></script>
+    <script src="{{ asset('backend/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js') }}"></script>
+    <script src="{{ asset('backend/lib/jquery.sparkline.bower/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('backend/lib/d3/d3.js') }}"></script>
+    <script src="{{ asset('backend/lib/rickshaw/rickshaw.min.js') }}"></script>
+    <script src="{{ asset('backend/lib/chart.js/Chart.js') }}"></script>
+    <script src="{{ asset('backend/lib/Flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('backend/lib/Flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('backend/lib/Flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('backend/lib/flot-spline/jquery.flot.spline.js') }}"></script>
+
+    <script src="{{ asset('backend/js/starlight.js') }}"></script>
+    <script src="{{ asset('backend/js/ResizeSensor.js') }}"></script>
+    <script src="{{ asset('backend/js/dashboard.js') }}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::namespace('Backend')->prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::get('/', function () {
     return view('frontend.home');
