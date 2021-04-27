5cf7dff0879438ad3876a7efb4b5588be6af4a2c
shahriarshaon <shahriarshaon5@gmail.com>
2021-04-27 16:13:21 +0600

    fixed some paramiter issues

--- app/Interface/RepoInterface.php
+++ app/Interface/RepoInterface.php
@@ -2,15 +2,11 @@
 
 namespace App\Interface;
 
-use App\Http\Requests\UpdateCategoryRequest;
-use App\Models\Category;
-use Illuminate\Http\Request;
-
 interface RepoInterface
 {
     public function index();
     public function store($request);
-    public function edit($slug);
+    public function edit($id);
     public function update($request, $category);
     public function destroy($id);
 }
