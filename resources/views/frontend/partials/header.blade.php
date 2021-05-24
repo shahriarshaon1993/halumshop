b5c13196a8e1aae838a87624fb42064fb1e82845
shahriarshaon <shahriarshaon5@gmail.com>
2021-05-25 02:37:13 +0600

    navbar added

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,5 +1,40 @@
     {{-- Start Header Section --}}
     <header class="header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container">
+                <a class="navbar-brand d-none d-sm-block flex-shrink-0" href="#">
+                    <img src="{{ asset('photos/logo-dark.png') }}" alt="halumshop" style="width: 142px;">
+                </a>
 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="input-group d-none d-lg-flex mx-4">
+                    <form action="" >
+                        <div class="input-group">
+                            <input type="text" class="form-control rounded-end" placeholder="Search for products">
+                            <div class="input-group-append">
+                                <button class="btn" type="submit">
+                                    <i class="fa fa-search" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
     {{-- Start Header Section --}}
