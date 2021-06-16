fbab35b97445bf8e5cf5f5cbb45be740ca42a856
shahriarshaon <shahriarshaon5@gmail.com>
2021-06-16 19:47:06 +0600

    fixed main.js bugs

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,18 +10,17 @@ closeBtn.addEventListener("click", () => {
     rightSidebar.classList.remove("change");
 });
 
-
 const signinBtn = document.querySelector('.signinBtn');
 const signupBtn = document.querySelector('.signupBtn');
 const formBx = document.querySelector('.login__container__formBx');
 const login = document.querySelector('.login');
 
-signupBtn.onclick = function() {
+signupBtn.addEventListener("click", () => {
     formBx.classList.add('active');
     login.classList.add('active');
-}
+});
 
-signinBtn.onclick = function() {
+signinBtn.addEventListener("click", () => {
     formBx.classList.remove('active');
     login.classList.remove('active');
-}
+});
