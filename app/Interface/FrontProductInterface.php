eb4ae24396fe448cc5f26159e3e20bf52b110fd9
shahriarshaon <shahriarshaon5@gmail.com>
2021-06-27 00:17:38 +0600

    fix bug FrontProductController

--- app/Interface/FrontProductInterface.php
+++ app/Interface/FrontProductInterface.php
@@ -6,5 +6,5 @@ interface FrontProductInterface
 {
     public function productView($slug);
     public function addProductCart($request, $slug);
-    public function category($slug)
+    public function category($slug);
 }
